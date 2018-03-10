fbcb9c7d6ed64d2991ebdfee6e97f1fd03ec2b5a
Jeremy Ryan Chandler <jeremy@MacBook-Pro-de-Jeremy.local>
2018-03-10 11:14:16 -0700

    gif stuff

--- index.js
+++ index.js
@@ -7,11 +7,28 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/gif', function (req, res){
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log('Searching for a gif...');
+    var query = req.query.query;
+    console.log('The query is ' + query);
+    var count = 0;
+    var url = gifSearch(req, res, query, function(req, res, url){
+        if (count > 2)
+          count = 0;
+        if (count == 0)
+          res.write("<div class=\"row\">");
+        res.write("<div class=\"col-sm-4\"><img src=\"" + url + "\" class=\"gif\"></div>");
+        if (count == 2)
+          res.write("</div>");
+        count++;
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
     var GphApiClient = require('giphy-js-sdk-core')
-    client = GphApiClient("")
+    client = GphApiClient("0AeM29IB0MkPlZDlBXgCKQlvZWGpm01J")
 
   var WebSocketServer = require('ws').Server,
       wss = new WebSocketServer({
@@ -33,13 +50,17 @@ express()
       });
   });
 
-//   /// Gif Search
-// client.search('gifs', {"q": "cats"})
-//   .then((response) => {
-//     response.data.forEach((gifObject) => {
-//       console.log(gifObject)
-//     })
-//   })
-//   .catch((err) => {
-//
-//   })
+function gifSearch(req, res, query, callback){
+  client.search('gifs', {"q": query})
+    .then((response) => {
+      response.data.forEach((gifObject) => {
+        var url = gifObject.images.fixed_height.gif_url;
+        console.log(url);
+        callback(req, res, url);
+      })
+      res.end();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
